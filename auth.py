abebabc59c72367bd4301c6ea3d61275fa22db67
AdminAcheTece <gestao.achetece@gmail.com>
2025-09-06 14:37:10 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,1 +1,10 @@
+# auth.py
+from flask import Blueprint, redirect, url_for
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login/google')
+def login_google():
+    # por enquanto só redireciona para o login normal
+    return redirect(url_for('login'))
 
