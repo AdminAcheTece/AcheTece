8a7ec5a9e1568fac10f2ecd7aa5c7f9d9e00978d
Hub Têxtil <gestao.achetece@gmail.com>
2026-09-02 17:40:53 -0300

    Delete reset_db.py

--- reset_db.py
+++
@@ -1,7 +0,0 @@
-from main import db, app
-
-with app.app_context():
-    db.drop_all()    # Apaga todas as tabelas existentes
-    db.create_all()  # Cria todas as tabelas com base nos modelos atuais
-
-print("✅ Banco de dados reiniciado com sucesso.")
